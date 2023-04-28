3663d599230be20e1bcdfa6b169643b884ffc3ec
Shivam Sharma <91273192+satshiv7983@users.noreply.github.com>
2023-04-28 21:59:14 +0530

    Create Maximum point you can obtain from cards.py

---
+++ Sliding Window and 2 Pointers/Maximum point you can obtain from cards.py
@@ -0,0 +1,14 @@
+
+class Solution:
+    def maxScore(self, cardPoints, n, k):
+        n = len(cardPoints)
+        res = sumi = sum(cardPoints[:k])
+        left = k-1
+        right = n-1
+        while(left>=0):
+            sumi-=cardPoints[left]
+            sumi+=cardPoints[right]
+            res = max(res,sumi)
+            left-=1
+            right-=1
+        return res
