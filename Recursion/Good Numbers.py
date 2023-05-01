ff89266ae1d2bd7f31dc7605beda1062c0ef089c
Shivam Sharma <91273192+satshiv7983@users.noreply.github.com>
2023-05-01 22:13:25 +0530

    Update Good Numbers.py

--- Recursion/Good Numbers.py
+++ Recursion/Good Numbers.py
@@ -18,3 +18,36 @@ class Solution:
             if ob.isValid(i,D)==True:
                 ans.append(i)
         return ans
+
+
+    # recursive approach
+    class Solution:
+    def goodNumbers(self,L,R,d):
+        def isValid(n, d,sum):
+            if n==0:
+                return True
+            digit = n % 10;
+            if (digit == d):
+                return False;
+                
+            if digit<=sum :
+                return False
+            
+            
+            return isValid(n//10,d,sum+digit)
+                
+        
+        ans=[]
+        for i in range(L, R + 1):
+            if i%10==0 :
+                if d!=0:
+                    i=i//10
+                    if(isValid(i, d,0)):
+                        ans+=i*10,
+                else:
+                    continue
+            else:
+                if(isValid(i, d,0)):
+                        ans+=i,
+                
+        return ans
