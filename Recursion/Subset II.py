d3edcc5d36396ebcf5e317db03afa8ec1ca1fd7c
Shivam Sharma <91273192+satshiv7983@users.noreply.github.com>
2023-05-02 22:48:40 +0530

    Create Subset II.py

---
+++ Recursion/Subset II.py
@@ -0,0 +1,22 @@
+class Solution:
+    def printUniqueSubset(self, nums):
+        res = []
+        nums.sort()
+
+        def backtrack(i, subset):
+            if i == len(nums):
+                res.append(subset[::])
+                return
+
+            # All subsets that include nums[i]
+            subset.append(nums[i])
+            backtrack(i + 1, subset)
+            subset.pop()
+            # All subsets that don't include nums[i]
+            while i + 1 < len(nums) and nums[i] == nums[i + 1]:
+                i += 1
+            backtrack(i + 1, subset)
+
+        backtrack(0, [])
+        res.sort()
+        return res
